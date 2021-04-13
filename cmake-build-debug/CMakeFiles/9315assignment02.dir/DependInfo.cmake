9be5ea5db2f87bd3f4747c9f62f8fadd8757a54c
xc-lin <736196315@qq.com>
2021-04-13 11:32:53 +0800

    add cmakelist

---
+++ cmake-build-debug/CMakeFiles/9315assignment02.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/a606/CLionProjects/9315assignment02/bits.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/bits.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/bsig.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/bsig.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/create.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/create.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/dump.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/dump.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/gendata.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/gendata.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/hash.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/hash.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/insert.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/insert.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/page.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/page.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/psig.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/psig.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/query.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/query.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/reln.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/reln.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/select.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/select.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/stats.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/stats.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/tsig.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/tsig.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/tuple.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/tuple.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/util.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/util.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/x1.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/x1.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/x2.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/x2.c.o"
+  "/Users/a606/CLionProjects/9315assignment02/x3.c" "/Users/a606/CLionProjects/9315assignment02/cmake-build-debug/CMakeFiles/9315assignment02.dir/x3.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/9315assignment02.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 9315assignment02.
+# This may be replaced when dependencies are built.
